9e2e466e9250674af6ed11384058bd65bfad0bd4
Munna, Partha-Mukherjee <partha-mukherjee.munna@hsrw.org>
2025-06-04 08:38:21 +0200

    minor update

--- api_main.py
+++ api_main.py
@@ -1,11 +1,13 @@
-# api_main.py
+#partha, This is the main entry point for the FastAPI application.
+
 from fastapi import FastAPI
 from routes import router
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-
+# Create an instance of the FastAPI app
+# This instance will be used to define and run the API
 app = FastAPI()
 
 # Include the routes from routes.py
